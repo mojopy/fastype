bba338335ed5a73a1e1c6ed9e03e5b975f93cd14
rufaidah3210 <rufaidah3210@gmail.com>
2024-12-07 13:30:30 +0600

    modularized

---
+++ constants.py
@@ -0,0 +1,8 @@
+# constants.py
+
+HIGH_SCORES_FILE = "high_scores.txt"
+LAST_LEVEL_FILE = "last_level.txt"
+FONT_SIZE = 1  # Variable for font size adjustment (demonstrative)
+BASE_SPEED = 500  # Base falling speed (ms)
+MILESTONE_SPEED_INCREMENT = 50  # Millisecond decrement for each milestone
+MILESTONES = [200, 500, 1000, 2000, 3000, 5000, 7000, 10000, 12000]
